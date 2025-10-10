496da9792ea201556fb2efd2eb44fd6f4d5fbb74
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-10-10 08:52:04 +0000

    Modifiche componenti

--- Pokemon/src/app/generazione1/generazione1.component.ts
+++ Pokemon/src/app/generazione1/generazione1.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../models/Pokemon.model';
 
 @Component({
   selector: 'app-generazione1',
@@ -9,14 +11,25 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
   imports: []
 })
 export class Generazione1Component implements OnInit {
-  pokemonList: string[] = [];
+  pokemonList: Pokemon[] = [];
+  loading!: boolean;
+  o!: Observable<Pokemon[]>;
 
   constructor(private http: HttpClient) {}
 
+  makeGet() {
+    this.loading = true;
+    this.o = this.http.get<Pokemon[]>("https://redesigned-disco-5ggww45w9rxxfq44-5000.app.github.dev/gen1"); // assicurati che endpoint sia corretto
+    this.o.subscribe(this.getData);
+  }
+
+  getData = (data: Pokemon[]) => {
+    this.pokemonList = data;
+    this.loading = false;
+    console.log(this.pokemonList);
+  }
+
   ngOnInit(): void {
-    this.http.get<string[]>('http://127.0.0.1:5000/gen1').subscribe({
-      next: data => this.pokemonList = data,
-      error: (err: HttpErrorResponse) => console.error('Errore caricamento Pokémon:', err)
-    });
+    this.makeGet();
   }
 }
---
+++ Pokemon/src/app/models/Pokemon.model.ts
@@ -0,0 +1,15 @@
+export interface Pokemon {
+  HP: number
+  attack: number
+  defence: number
+  generation: number
+  id: number
+  legendary: number
+  name: string
+  sp_atk: number
+  sp_def: number
+  speed: number
+  total: number
+  type1: string
+  type2: string
+}
